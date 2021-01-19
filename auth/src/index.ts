d6eacc16b94debc6c8cff5e58124d39ebe3385ca
pantaLuc <lucapameni@gmail.com>
2021-01-19 10:57:18 +0100

    manager les routes dans mon microservices ingress-nginx pour lancer le serveur localement

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import {json} from 'body-parser';
+import { currentUserRoute } from './routes/current-user';
 
 const app= express();
 app.use(json());
+app.use(currentUserRoute);
 
-app.listen(3000 , ()=>{console.log("auth app is listening to port 3000")
+app.listen(4000, ()=>{console.log("auth app is listening to port 4000 !!!")
 
 });
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router=express.Router()
+
+router.post('/api/users/signup',(req,res)=>{
+
+
+});
+
+export{router as signupRouter}
