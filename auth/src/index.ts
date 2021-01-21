0acd6b6599e6feccdcd3da8f0e2de7e761f3f59e
pantaLuc <lucapameni@gmail.com>
2021-01-21 15:51:08 +0100

    working on signup and error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import {json} from 'body-parser';
 import { currentUserRoute } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app= express();
 app.use(json());
 app.use(currentUserRoute);
+app.use(signinRouter)
+app.use(signoutRouter);
+app.use(signupRouter)
 
 app.listen(4000, ()=>{console.log("auth app is listening to port 4000 !!!")
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,26 @@
-import express from 'express'
+import express, {Request ,Response} from 'express'
+import {body,validationResult} from 'express-validator'
 
 const router=express.Router()
 
-router.post('/api/users/signup',(req,res)=>{
-
+router.post('/api/users/signup',[
+  body('email')
+  .isEmail()
+  .withMessage("Email must be valid"),
+  body("password")
+  .trim()
+  .isLength({min:4 , max:20})
+  .withMessage("password must be between 4 and 20characters")
+],
+(req:Request,res:Response)=>{
+  const {email ,password}=req.body
+  const error=validationResult(req);
+  if(!error.isEmpty()){
+    res.status(400).send(error.array());
+  }
+  console.log("creating a User .....")
+  res.send({})
+  
 
 });
 
