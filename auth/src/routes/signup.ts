133ca406117baacfb44b4042f45bead8a468237e
pantaLuc <lucapameni@gmail.com>
2021-02-01 11:08:26 +0100

    adding a jwt in our auth service

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,7 @@ import {body,validationResult} from 'express-validator'
 import {RequestValidationError} from '../errors/req-validation-error';
 import 'express-async-errors'
 import { BadRequesError } from '../errors/bad-reques';
+import jwt from 'jsonwebtoken'; 
 
 const router=express.Router()
 
@@ -33,6 +34,15 @@ router.post('/api/users/signup',[
   const user=User.build({email ,password})
   //save the user to the data base
   await user.save();
+  //Generate JWT
+  const userJwt=jwt.sign({
+    id:user.id,
+    email:user.email
+  },'panta') 
+  //Store it on session objet 
+  req.session={
+    jwt:userJwt
+  };
   
   res.status(201).send(user);
 
