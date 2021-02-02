93af39dc67050d10d6661ce0eedfbebfa6a1ec01
pantaLuc <lucapameni@gmail.com>
2021-02-02 04:29:13 +0100

    implementing siginin finish

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express ,{Request ,Response} from 'express'
-import {body, validationResult} from 'express-validator';
-import { RequestValidationError } from '../errors/req-validation-error';
+import {body} from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequesError } from '../errors/bad-reques';
 import { validateRequest } from '../middleware/valid-request';
+import { User } from '../models/user';
+import { Password } from '../utilities/pass-hashing';
 const router =express.Router();
 
 router.post('/api/users/signin',[
@@ -14,9 +17,34 @@ router.post('/api/users/signin',[
    .withMessage("password must be between 4 and 20 characters")
 ],
 validateRequest,
-(req:Request ,res:Response)=>{
-  
+async (req:Request ,res:Response)=>{
+   const {email , password}=req.body
+   const existingUser=await User.findOne({email})
+   if(!existingUser){
+     throw new BadRequesError("invalid credential")
+   }
+
+   const passMatch= await Password.compareHash(
+     existingUser.password ,
+     password);
+  if(!passMatch){
+    throw new BadRequesError('invalid Password');
+  }
+  // generate JWT
+  const userJwt=jwt.sign(
+    {
+      id:existingUser.id,
+      email:existingUser.email
+
+    },
+    process.env.JWT_KEY ! /// pour dire ne te soucis pas du type j ' ai gerer dans index.js
     
+  )
+  //store it on session 
+  req.session={
+    jwt:userJwt
+  }
+  res.status(200).send(existingUser)
 });
 
 export{ router as signinRouter}
