63558f444ba23f298d89f00e6e884b5d7fcda2a0
pantaLuc <lucapameni@gmail.com>
2021-02-03 13:24:36 +0100

    implementing current-user and signout function

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken'
 const router=express.Router();
 
 router.get('/api/users/currentuser' ,(req,res)=>{
-    res.send("Luc Panta vous ecoute")
+    if(!req.session||!req.session.jwt!){
+       return res.send({currentuser:null})
+    }
+
+    try {
+        const payload=jwt.verify(
+            req.session.jwt ,
+            process.env.JWT_KEY!);
+            res.send({currentuser:payload})
+    } catch (error) {
+       res.send({currentuser:null}) 
+    }
+    
+
 });
 
 export{router as currentUserRoute}
