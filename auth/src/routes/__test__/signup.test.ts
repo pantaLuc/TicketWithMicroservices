47fe4b3224c2e9947b89b1ea86e2409ce2d80073
pantaLuc <lucapameni@gmail.com>
2021-02-08 17:26:32 +0100

    changing cookiesession parameter secure to be able proceed changewhen making test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import {app} from '../../app'
+import { Password } from '../../utilities/pass-hashing';
 
 it('returns a 201  on succesfull signup' , async ()=>{
 
@@ -11,4 +12,67 @@ it('returns a 201  on succesfull signup' , async ()=>{
             })
             .expect(201);
 
+});
+
+it('return a 400 on with an invalid email' , async ()=>{
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+           email:'pantalucgmail.com',
+           password:'password'
+        }).expect(400);
+});
+
+it('return a 400  with an invalid password' , async ()=>{
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+           email:'pantaluc@gmail.com',
+           password:'pa'
+        }).expect(400);
+})
+
+it('return a 400  with  missing email and password ' , async ()=>{
+    await request(app)
+                  .post('/ai/users/signup')
+                  .send({
+                       email:'lucapameni@gmail.com',
+                       password:'p'  
+                  }).expect(400);   
+    
+     await request(app)
+        .post('/api/users/signup')
+        .send({
+           email:'',
+           password:'panta'
+        }).expect(400);
+}) ;
+
+it('non duplicated email' ,async ()=>{
+
+    await request(app)
+            .post('api/users/signup')
+            .send({
+                email:'pantaluc@gmail.com',
+                password:'panta',
+            }).expect(201)
+
+    await request(app)
+            .post('api/users/signup')
+            .send({
+                email:'pantaluc@gmail.com',
+                password:'panta'
+            }).expect(400)
+});
+it(' check the cookies ', async ()=>{
+   
+    const response=await request(app)
+                         .post('api/users/signup')
+                         .send({
+                             email:'pantaluc@gmail.com',
+                             password:'panta'
+                         })
+                         .expect(201);
+                
+     expect(response.get('Set-Cookie')).toBeCalled()   
 })
