9ce77fb4d7a5e40078117c3169b39336a5fb4d09
pantaLuc <lucapameni@gmail.com>
2021-02-08 04:20:54 +0100

    editing package.json to ad test on script and describe jest also creatin test folder and setup.ts to setup our test

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import mongoose from 'mongoose'
+import {app} from '../app';
+// est un hook qui va s' executer avant tout nos tests
+let mongo:any
+beforeAll(async ()=>{
+    mongo= new  MongoMemoryServer() ;//create a mmongomemory server
+    const mongoUri=await mongo.getUri();
+
+    await mongoose.connect(mongoUri,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+    });
+});
+//un hook qui va s' executer avant chaque test individuel
+beforeEach(async ()=>{
+    const collections=await mongoose.connection.db.collections();
+
+    for(let collection of collections){
+        await collection.deleteMany({})
+    }
+});
+
+// un hook qui va s' executer apres que tout nos test ce soit fait 
+
+afterAll(async ()=>{
+   await mongo.stop();
+   await mongoose.connection.close()
+})
